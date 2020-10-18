7f59d55d79ff986f395d45f063312fccd29a6928
Lincoln S. Matra <lmatra@bentley.edu>
2020-10-18 13:56:43 -0400

    Initial commit

---
+++ app/controllers/HomeController.scala
@@ -0,0 +1,56 @@
+package controllers
+
+import javax.inject.Inject
+import play.api._
+import play.api.mvc._
+import play.api.i18n._
+
+/**
+ * This controller creates an `Action` to handle HTTP requests to the
+ * application's home page.
+ */
+//@Singleton
+class HomeController @Inject() (cc: ControllerComponents) extends AbstractController(cc) with I18nSupport {
+     
+  
+  //Setup an application logger
+  val appLogger: Logger = Logger("application")
+   
+  /**
+   * Create an Action to render an HTML page.
+   *
+   * The configuration in the `routes` file means that this method
+   * will be called when the application receives a `GET` request with
+   * a path of `/`.
+   */
+  def index() = Action { implicit request: Request[AnyContent] =>
+    val messages: Messages = request.messages
+    val title: String = messages("home.title")  
+ 
+    appLogger.info("Debug "+title)    
+    Ok(views.html.index("Welcome"))
+  }
+
+  //  def about() = Action { implicit request: Request[AnyContent] =>
+  //    Ok(views.html.about("About"))
+  //  }
+  //
+  //  def products() = Action { implicit request: Request[AnyContent] =>
+  //    Ok(views.html.products("Products"))
+  //  }
+
+  /***
+  *  We might need to use this function to load certain pages.
+  */
+  def loadPage(page: String) = Action { implicit request: Request[AnyContent] =>
+    appLogger.debug(s"Loading page: $page")
+    
+    page match {
+     // case "about"    => Ok(views.html.about(page.capitalize))
+     // case "products" => Ok(views.html.products(page.capitalize))
+      case _          => Ok(views.html.index("Welcome"))
+    }
+  }
+  
+
+}
